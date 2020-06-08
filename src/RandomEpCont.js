91fc5a72605b4a336f30d5b5ecf1b3a6d55c589b
Hope <hopegiometti@gmail.com>
2020-06-07 22:47:37 -0400

    added episode details

--- src/RandomEpCont.js
+++ src/RandomEpCont.js
@@ -4,6 +4,7 @@ export const RandomEpCont = (displayEp) => {
     return ( <>
         {console.log(Object.values(displayEp)[0].episodeName)}
         <h1>{Object.values(displayEp)[0].episodeName}</h1>
+        <h4>Season: {Object.values(displayEp)[0].airedSeason}, Episode Number: {Object.values(displayEp)[0].airedEpisodeNumber}</h4>
     </> )
 }
 
