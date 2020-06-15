6adae411edd04ed86b0473281a9135130c73a6c4
Hope <hopegiometti@gmail.com>
2020-06-15 13:01:20 -0400

    updated hover features

--- src/ShuffleButton.js
+++ src/ShuffleButton.js
@@ -25,11 +25,21 @@ export default class ShuffleButton extends React.Component {
             height: 700px;
         `
 
+        let StyledImage = styled.img`
+            display: inline-block;
+        `
+        let StyledHOne = styled.h1`
+            position: absolute;
+            top: 50%;
+            left: 50%;
+        `
+
         return(<>
             <StyledDiv onMouseEnter={this.onMouseOverHandler} onMouseLeave={this.onMouseOverHandler}>
-                <StyledButton onClick={() => this.props.shuffleEpisodes(this.props.allEps)}><img src={PrisonMike} alt="photo of prison mike"/></StyledButton>
+                {/* <StyledButton onClick={() => this.props.shuffleEpisodes(this.props.allEps)}><img src={PrisonMike} alt="photo of prison mike"/></StyledButton> */}
+                {this.state.hover ? <StyledButton onClick={() => this.props.shuffleEpisodes(this.props.allEps)}><StyledImage src={PrisonMike} alt="photo of prison mike"/><StyledHOne>Click Here to Shuffle!</StyledHOne></StyledButton> : <StyledButton onClick={() => this.props.shuffleEpisodes(this.props.allEps)}><StyledImage src={PrisonMike} alt="photo of prison mike"/></StyledButton>}
             </StyledDiv>
         </>)
     }
 }
-// {this.state.hover ? <StyledButton onClick={() => this.props.shuffleEpisodes(this.props.allEps)}><img src={PrisonMike} alt="photo of prison mike"/></StyledButton>}
+// {this.state.hover ? <StyledButton onClick={() => this.props.shuffleEpisodes(this.props.allEps)}><img src={PrisonMike} alt="photo of prison mike"/>Click Here to Shuffle!</StyledButton> : <StyledButton onClick={() => this.props.shuffleEpisodes(this.props.allEps)}><img src={PrisonMike} alt="photo of prison mike"/></StyledButton>}
